6048a7bc19c4a174e6629a70f214b5fbafd26aa5
thesneath <71525393+thesneath@users.noreply.github.com>
2021-03-18 11:36:33 -0700

    created first pokemonList in scripts.js
    Removed favoriteFood content from exercise 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-alert('Hello world');
-
-let favoriteFood = 'Mac and Cheese';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 2.33,
+        type: ['Grass', 'Poison']
+    },
+    {
+        name: 'Charmander',
+        height: 2,
+        type: ['Fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 1.67,
+        type: ['Water']
+    },
+    {
+        name: 'Pikachu',
+        height: 1.33,
+        type: ['Electric']
+    },
+    {
+        name: 'Eevee',
+        height: 1,
+        type: ['Normal']
+    },
+    {
+        name: 'Dragonite',
+        height: 7.25,
+        type: ['Dragon', 'Flying']
+    } 
+];
