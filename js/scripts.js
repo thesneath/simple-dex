b9e3bcb493099aacc6dc4462b769b97d05c3cd6d
thesneath <71525393+thesneath@users.noreply.github.com>
2021-03-19 15:20:22 -0700

    Refactored for loop to a forEach, created pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,61 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 2.33,
-        type: ['Grass', 'Poison']
-    },
-    {
-        name: 'Charmander',
-        height: 2,
-        type: ['Fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 1.67,
-        type: ['Water']
-    },
-    {
-        name: 'Pikachu',
-        height: 1.33,
-        type: ['Electric']
-    },
-    {
-        name: 'Eevee',
-        height: 1,
-        type: ['Normal']
-    },
-    {
-        name: 'Dragonite',
-        height: 7.25,
-        type: ['Dragon', 'Flying']
-    } 
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 2.33,
+            type: ['Grass', 'Poison']
+        },
+        {
+            name: 'Charmander',
+            height: 2,
+            type: ['Fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 1.67,
+            type: ['Water']
+        },
+        {
+            name: 'Pikachu',
+            height: 1.33,
+            type: ['Electric']
+        },
+        {
+            name: 'Eevee',
+            height: 1,
+            type: ['Normal']
+        },
+        {
+            name: 'Dragonite',
+            height: 7.25,
+            type: ['Dragon', 'Flying']
+        } 
+    ];
+
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    function getAll(){
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 // this loops through the pokemonList array and prints a <div> to the DOM containing: "'pokemon name' (height: 'height')" 
 
 document.write("<div class=\"text\">");
-for(i = 0; i < pokemonList.length; i++) {
-    if(pokemonList[i].height > 5){
-        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Crikey, look at the size of this one!</p>`)
+
+pokemonRepository.getAll().forEach(pokemon => {
+    if(pokemon.height > 5){
+        document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - Crikey, look at the size of this one! <p>`)
     } else {
-        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
+        document.write(`<p> ${pokemon.name} (height: ${pokemon.height})<p>`)
     }
-}
+});
+
 document.write("</div>");
